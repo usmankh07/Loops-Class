64906f95354dab00861dccf4de960164e83018a3
Haris Ali <75772752+Harisali78@users.noreply.github.com>
2022-07-03 04:18:32 -0700

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,43 @@
+// for(a=0; a<5; a++){
+//     console.log("The value is ", a);
+// }
+
+// let myFriends = ["Ali", "Ahmad", "Ayaan", "Anas"];
+// for (let index = 0; index < myFriends.length; index++) {
+//     console.log("Hello, How are you ", myFriends[index])
+// }
+// myFriends.forEach(ali => {
+//     console.log("Hello, what's up ", ali);
+// });
+
+// for (const element of myFriends) {
+//     console.log("Hello", element, "by using modern javascript");
+// }
+// let myInfo = {
+//     name:"Haris",
+//     age:36,
+//     batch:"A 13"
+// }
+// for (let element in myInfo) {
+//     console.log(`The ${element} of myInfo is ${myInfo[element]}`);
+// }
+
+
+// While Loop
+// let i = 55;
+// while (i<10) {
+//     console.log("The value of i is ", i);
+//     i++
+// }
+
+// do {
+//     console.log("The value is less than ", i);
+//     i++
+// } while (i<5);
+
+console.log("Hello Haris");
+setTimeout(() => {
+    console.log("Hello Haris by set time out");
+}, 0);
+
+console.log("Hello Haris outside the set time out");
